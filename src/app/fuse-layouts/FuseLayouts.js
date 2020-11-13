f1f293bb21ca3a4c134f8e25e0dc808a17a1d546
Zach Peltzer <zach.peltzer@nxtailab.com>
2020-11-13 11:54:09 -0600

    Set basic branding aspects
    - Set `favicon.ico` and page title
    - Remove extra themes/layouts and theming/layout configuration sidebar
    - Remove footer
    - Set colors for the dark and light themes
    - Replace FUSE logo with NxtSuite logo

--- src/app/fuse-layouts/FuseLayouts.js
+++ src/app/fuse-layouts/FuseLayouts.js
@@ -1,11 +1,7 @@
-import layout1 from 'app/fuse-layouts/layout1/Layout1';
-import layout2 from 'app/fuse-layouts/layout2/Layout2';
-import layout3 from 'app/fuse-layouts/layout3/Layout3';
+import main from 'app/fuse-layouts/main-layout/MainLayout';
 
 const FuseLayouts = {
-	layout1,
-	layout2,
-	layout3
+	main,
 };
 
 export default FuseLayouts;
