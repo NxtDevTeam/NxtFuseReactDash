29d1772fb9010814e3e92bb6703ba42bebc1fbb7
Zach Peltzer <zach.peltzer@nxtailab.com>
2020-11-12 19:51:00 -0600

    Add redux slice for organization member lists

--- src/app/store/organization/index.js
+++ src/app/store/organization/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import organizations from './organizationsSlice';
+import members from './membersSlice';
 
 const organizationReducers = combineReducers({
 	organizations,
+	members,
 });
 
 export default organizationReducers;
