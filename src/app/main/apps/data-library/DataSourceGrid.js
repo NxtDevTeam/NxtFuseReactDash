facbb76c66f1bfbd68aab4b3beae7ef416dbc126
Zach Peltzer <zach.peltzer@nxtailab.com>
2020-12-04 17:20:19 -0600

    Allow data source selection from grid
    - Clicking on a data source item in the grid selects it in the side panel
    - Selceted grid card is raised

--- src/app/main/apps/data-library/DataSourceGrid.js
+++ src/app/main/apps/data-library/DataSourceGrid.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { Card, CardContent, Grid, Icon, Typography } from '@material-ui/core';
+import React, { useCallback } from 'react';
+import {
+	Card,
+	CardActionArea,
+	CardContent,
+	Grid,
+	Icon,
+	Typography,
+} from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import { getSourceTypeData } from './dataSourceUtils';
+import {
+	selectSelectedItemId,
+	setSelectedItem,
+} from './store/dataSourcesSlice';
 
-function DataSourceCard({ type, name }) {
+function DataSourceCard({ selected, id, type, name }) {
+	const dispatch = useDispatch();
 	const { name: typeName, icon } = getSourceTypeData(type);
 
+	const handleClick = useCallback(
+		() => dispatch(setSelectedItem(id)),
+		[dispatch, id]);
+
 	return (
-		<Card>
-			<CardContent className="flex flex-col items-center">
-				<Typography>{typeName}</Typography>
-				<Icon className="text-48">{icon}</Icon>
-				<Typography variant="caption">{name}</Typography>
-			</CardContent>
+		<Card raised={selected}>
+			<CardActionArea onClick={handleClick}>
+				<CardContent className="flex flex-col items-center">
+					<Typography>{typeName}</Typography>
+					<Icon className="text-48">{icon}</Icon>
+					<Typography variant="caption">{name}</Typography>
+				</CardContent>
+			</CardActionArea>
 		</Card>
 	);
 }
 
 function DataSourceGrid({ dataSources, className }) {
+	const selectedItemId = useSelector(selectSelectedItemId);
+
 	return (
 		<Grid container spacing={1} className={className}>
 			{dataSources.map(({ id, type, name }) => (
@@ -27,7 +48,12 @@ function DataSourceGrid({ dataSources, className }) {
 					sm={4}
 					md={2}
 				>
-					<DataSourceCard type={type} name={name} />
+					<DataSourceCard
+						selected={id === selectedItemId}
+						id={id}
+						type={type}
+						name={name}
+					/>
 				</Grid>
 			))}
 		</Grid>
